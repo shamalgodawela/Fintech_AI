4f6e03f0a0244eaef8a174b254a2f919705505b3
G.Gimsara <lenmicr250@gmail.com>
2025-04-28 23:57:18 +0530

    done

--- frontend/src/pages/UpdateBudget.jsx
+++ frontend/src/pages/UpdateBudget.jsx
@@ -216,6 +216,7 @@ const UpdateBudget = () => {
               className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
             >
               Update Budget
+              
             </button>
           </form>
         </div>
